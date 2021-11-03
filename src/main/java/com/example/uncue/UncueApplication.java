4bd92c23d97debd2ee72f63356a6bee96d6d3bc2
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2021-11-03 14:19:25 +0530

    Initialized spring project

---
+++ src/main/java/com/example/uncue/UncueApplication.java
@@ -0,0 +1,13 @@
+package com.example.uncue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uncue/UncueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uncue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
